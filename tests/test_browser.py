72d8b005819df7e2311a2248c6d6123f7767afaf
Andrews Medina <andrewsmedina@gmail.com>
2013-01-15 22:11:57 -0200

    Removed patchs and tests related zope.testbrowser.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -17,20 +17,6 @@ from fake_webapp import EXAMPLE_APP
 
 class BrowserTest(unittest.TestCase):
 
-    def patch_driver(self, pattern):
-        self.old_import = __builtins__.__import__
-
-        def custom_import(name, *args, **kwargs):
-            if pattern in name:
-                return None
-            return self.old_import(name, *args, **kwargs)
-
-        __builtins__.__import__ = custom_import
-
-    def unpatch_driver(self, module):
-        __builtins__.__import__ = self.old_import
-        reload(module)
-
     def browser_can_change_user_agent(self, webdriver):
         from splinter import Browser
         browser = Browser(driver_name=webdriver, user_agent="iphone")
@@ -42,13 +28,6 @@ class BrowserTest(unittest.TestCase):
 
     def test_brower_can_still_be_imported_from_splinters_browser_module(self):
         from splinter.browser import Browser
-
-    def test_should_work_even_without_zope_testbrowser(self):
-        self.patch_driver('zope')
-        from splinter import browser
-        reload(browser)
-        self.assertNotIn('zope.testbrowser', browser._DRIVERS)
-        self.unpatch_driver(browser)
 
     def test_should_raise_an_exception_when_browser_driver_is_not_found(self):
         with self.assertRaises(DriverNotFoundError):
